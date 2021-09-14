3fd8a97ebfffb89746321d54bd7cd19be8412bda
David Carpenter <carpenterd777@gmail.com>
2021-09-14 17:36:33 -0400

    add linting stuff

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+	root: true,
+	plugins: ["prettier", "@typescript-eslint"],
+	env: {
+		browser: true,
+	},
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: "module",
+	},
+	extends: [
+		"prettier",
+		"plugin:prettier/recommended",
+		"plugin:@typescript-eslint/recommended",
+	],
+	overrides: [
+		{
+			files: ["*.ts"],
+			extends: ["plugin:@typescript-eslint/recommended-requiring-type-checking"],
+			parserOptions: {
+				project: ["./tsconfig.json"],
+			},
+			rules: {
+				"@typescript-eslint/switch-exhaustiveness-check": "error",
+			},
+		},
+	],
+	// Do NOT use any eslint ruels that affect code formatting because prettier handles that.
+	rules: {
+		"no-console": "error",
+		"no-debugger": "error",
+		"no-var": "error",
+		"eqeqeq": "error",
+		"no-restricted-properties": [
+			"error",
+			{
+				object: "document",
+				property: "title",
+			},
+		],
+		"react/no-unescaped-entities": "warn",
+		"prettier/prop-types": "off",
+		"react/prefer-stateless-function": "error",
+		"@typescript-eslint/no-var-requires": "error",
+		"@typescript-eslint/adjacent-overload-signatures": "error",
+		"@typescript-eslint/no-extra-semi": "off",
+	},
+};
